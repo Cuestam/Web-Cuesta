c362647adc550d07e7f0b665f66ac3e34c618467
Mauro Cuesta <cuestamauro5@gmail.com>
2021-09-17 20:35:10 -0300

    Correciones en sass

--- sender.js
+++ sender.js
@@ -43,4 +43,18 @@ function sender(nombre, telefono, email, mensaje) {
 }
 
 
+// ANIMACION NAVBAR
+let ubicacionPrincipal = window.pageYOffset;
+
+window.addEventListener("scroll", function () {
+    let desplazamientoActual = window.pageYOffset;
+    if (ubicacionPrincipal >= desplazamientoActual) {
+        document.getElementsByTagName("nav")[0].style.top = "0px"
+    } else {
+        document.getElementsByTagName("nav")[0].style.top = "-100px"
+    }
+    ubicacionPrincipal = desplazamientoActual;
+})
+
+
 
